74a283cafa4fb03e24b84a8231b8915ecdd36e6c
PrakGov <55034062+prak112@users.noreply.github.com>
2024-11-12 14:50:14 +0200

    practice: Add pseudo code and unguided trial v1.0

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,38 @@
-export default function bubble_sort(arr: number[]): void {
+export default function bubble_sort(arr: number[]): number[] {
+    /**
+     * PSEUDO CODE
+     * -- My Approach v1.0 -- -> O(n^2)
+     * Sorting condition, Xi <= Xi+1
+         
+    startIndex = 0 // InnerLoop - increment after verifying sorting condition
+    subsetLimiter = 0 // OuterLoop - increment after complete array iteration
 
+    for(subsetLimiter = 0; subsetLimiter !== arr.length; subsetLimiter++)
+        endIndex = arr.length - subsetLimiter
+
+        for(startIndex = 0, startIndex !== endIndex; startIndex++ )
+            // sorting condition
+            if arr[startIndex] > arr[startIndex + 1]
+                temp = arr[startIndex]
+                arr[startIndex] = arr[startIndex + 1]
+                arr[startIndex + 1] = temp
+
+     * In subsequent iterations, sorting condition applied to sub-array, i.e., startIndex remains constant, endIndex keeps reducing after every iteration.
+     */
+    console.log('Unsorted array : ', arr)
+
+    for(let subsetLimiter = 0; subsetLimiter < arr.length; subsetLimiter++){
+        let endIndex = arr.length - subsetLimiter
+
+        for(let startIndex = 0; startIndex !== endIndex; startIndex++){
+            if(arr[startIndex] > arr[startIndex + 1]){
+                let temp = arr[startIndex]
+                arr[startIndex] = arr[startIndex + 1]
+                arr[startIndex + 1] = temp
+            }
+        }
+    }
+
+    console.log('Sorted array: ', arr)
+    return arr
 }
